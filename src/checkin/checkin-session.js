c1ace3ce9187781f71de3b6efa2a12dfb3a004e7
924lyd <laiyangde@gao7.com>
2025-12-12 08:57:42 +0800

    feat：忽略证书错误

--- src/checkin/checkin-session.js
+++ src/checkin/checkin-session.js
@@ -45,6 +45,7 @@ class AnyRouterSessionSignIn {
 				userAgent:
 					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
 				viewport: { width: 1920, height: 1080 },
+				ignoreHTTPSErrors: true, // 忽略HTTPS证书错误
 				args: [
 					'--disable-blink-features=AutomationControlled',
 					'--disable-dev-shm-usage',
